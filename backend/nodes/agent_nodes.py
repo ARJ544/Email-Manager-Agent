8289e79e089ee0600aae26defab6fd884d26f40e
Abhinav <arj43821@gmail.com>
2025-12-04 16:37:06 +0530

    setup nodes

---
+++ backend/nodes/agent_nodes.py
@@ -0,0 +1,64 @@
+from langgraph.graph import MessagesState
+from langchain_core.messages import SystemMessage, ToolMessage, AIMessage
+from config import llm_with_tools
+
+
+system_msg = SystemMessage(content=
+    f"You are Email Manager Agent, an agentic AI created by Abhinav Ranjan Jha. "
+    "You can fetch all the emails of user with query. "
+    "Also can delete the list of selected emails. "
+    "Your work is to call correct tools with correct agrs and name. and nothing else."
+)
+
+def call_llm_node(state: MessagesState):
+    
+    messages = state["messages"]
+    if not any(msg.__class__.__name__ == "SystemMessage" for msg in messages):
+        messages.insert(0, system_msg)
+        
+    response = llm_with_tools.invoke(messages)
+    return {"messages": [response]}
+
+def execute_tool_calls_node(state: MessagesState):
+    """
+    Executes all tool calls from the last message dynamically.
+
+    Args:
+        state (MessagesState): The current state containing messages and tool calls.
+
+    Returns:
+        dict: A dictionary with 'messages' containing the outputs of all executed tools.
+    """
+    last_message = state["messages"][-1]
+    tool_outputs = []
+
+    tools_map = {
+        # "": 
+    }
+
+    for tool_call in last_message.tool_calls:
+        tool_name = tool_call["name"]
+        args = tool_call["args"]
+
+        if tool_name in tools_map:
+            try:
+                result = tools_map[tool_name].invoke(args)
+                tool_outputs.append(ToolMessage(tool_call_id=tool_call['id'], content=str(result)))
+            except Exception as e:
+                tool_outputs.append(
+                    ToolMessage(tool_call_id=tool_call['id'], content=f"Error executing {tool_name}: {e}")
+                )
+        else:
+            print(f"No tool found with name '{tool_name}'")
+            tool_outputs.append(
+                ToolMessage(tool_call_id=tool_call['id'], content=f"No tool found with name '{tool_name}'")
+            )
+
+    return {"messages": tool_outputs}
+
+def should_call_tools(state: MessagesState):
+    last_message = state["messages"][-1]
+    if isinstance(last_message, AIMessage) and last_message.tool_calls:
+        return "tools"
+    else:
+        return "end"
