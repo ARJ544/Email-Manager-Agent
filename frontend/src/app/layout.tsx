e29942a7ae449f4e7edec32fd6a3e955058a8075
Abhinav <arj43821@gmail.com>
2025-12-05 20:00:14 +0530

    remove padding added bg colors

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
           >
             <NavigationBar />
 
-          <main className="pt-4">{children}</main>
+          <main className="scroll-smooth bg-gray-50 dark:bg-gray-900 min-h-screen">{children}</main>
 
         </ThemeProvider>
       </body>
