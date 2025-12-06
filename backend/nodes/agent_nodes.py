702af15178b806eb7c80f747cbe5e9b37cfef32e
Abhinav <arj43821@gmail.com>
2025-12-06 18:40:39 +0530

    added graph's state AgentState

--- backend/nodes/agent_nodes.py
+++ backend/nodes/agent_nodes.py
@@ -1,25 +1,35 @@
 from langgraph.graph import MessagesState
 from langchain_core.messages import SystemMessage, ToolMessage, AIMessage
 from config_llm import llm_with_tools
+from utils import get_list_of_emails_tool as gloe
+import graph.graph as grhp
+from langchain_core.messages import BaseMessage
 
 
 system_msg = SystemMessage(content=
-    f"You are Email Manager Agent, an agentic AI created by Abhinav Ranjan Jha. "
-    "You can fetch all the emails of user with query. "
-    "Also can delete the list of selected emails. "
-    "Your work is to call correct tools with correct agrs and name. and nothing else."
+    "You are Email Manager Agent, created by Abhinav. "
+    "You can fetch all emails based on queries. "
+    "The user never needs to provide access tokens or credentials. "
+    "The backend securely provides authentication tokens automatically. "
+    "When the user asks for an operation, only request the required search query, "
+    "and call the appropriate tool with the arguments. "
+    "Never ask the user for access_token or credentials."
 )
 
-def call_llm_node(state: MessagesState):
+def call_llm_node(state: grhp.AgentState):
     
     messages = state["messages"]
     if not any(msg.__class__.__name__ == "SystemMessage" for msg in messages):
         messages.insert(0, system_msg)
         
     response = llm_with_tools.invoke(messages)
-    return {"messages": [response]}
+    return {
+        "messages": [response],
+        "access_token": state["access_token"]  # KEEP TOKEN
+    }
 
-def execute_tool_calls_node(state: MessagesState):
+
+def execute_tool_calls_node(state: grhp.AgentState):
     """
     Executes all tool calls from the last message dynamically.
 
@@ -33,17 +43,18 @@ def execute_tool_calls_node(state: MessagesState):
     tool_outputs = []
 
     tools_map = {
-        # "": 
+        "get_list_of_emails": gloe.get_list_of_emails 
     }
 
     for tool_call in last_message.tool_calls:
         tool_name = tool_call["name"]
         args = tool_call["args"]
+        args["access_token"] = state["access_token"]
 
         if tool_name in tools_map:
             try:
                 result = tools_map[tool_name].invoke(args)
-                tool_outputs.append(ToolMessage(tool_call_id=tool_call['id'], content=str(result)))
+                tool_outputs.append(ToolMessage(tool_call_id=tool_call['id'], content=result))
             except Exception as e:
                 tool_outputs.append(
                     ToolMessage(tool_call_id=tool_call['id'], content=f"Error executing {tool_name}: {e}")
@@ -54,9 +65,12 @@ def execute_tool_calls_node(state: MessagesState):
                 ToolMessage(tool_call_id=tool_call['id'], content=f"No tool found with name '{tool_name}'")
             )
 
-    return {"messages": tool_outputs}
+    return {
+        "messages": tool_outputs,
+        "access_token": state["access_token"]  # KEEP TOKEN
+    }
 
-def should_call_tools(state: MessagesState):
+def should_call_tools(state: grhp.AgentState):
     last_message = state["messages"][-1]
     if isinstance(last_message, AIMessage) and last_message.tool_calls:
         return "tools"
